a753be815580dd1ca437d6c50179f013cf1f1dd0
Skip <skiphog@mail.ru>
2017-12-21 17:56:35 +0300

    add easin

--- assets/js/app.js
+++ assets/js/app.js
@@ -293,3 +293,31 @@ $('.map-container').one('touchstart dragstart click', function () {
     });
 });
 
+$('body').scrollspy({
+  target: '.navbar-fixed-top',
+  offset: 60,
+});
+
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+jQuery.extend(jQuery.easing,
+  {
+    def: 'easeOutQuad',
+    swing: function (x, t, b, c, d) {
+      return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
+    },
+    easeInOutExpo: function (x, t, b, c, d) {
+      if (t == 0) return b;
+      if (t == d) return b + c;
+      if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+      return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    },
+  });
+
+$('a.page-scroll').bind('click', function (e) {
+  e.preventDefault();
+  const that = $(this);
+  $('html, body').stop().animate({
+    scrollTop: $(that.attr('href')).offset().top - 60,
+  }, 1000, 'easeInOutExpo');
+});
+
--- public/index.php
+++ public/index.php
@@ -23,11 +23,11 @@
             <div class="navbar-collapse collapse" id="navbar">
 
                 <ul class="nav navbar-nav">
-                    <li><a href="#cho-v-kapcho">Чо в Капчо?</a></li>
-                    <li><a href="#kapcho-gallery">Галерея</a></li>
-                    <li><a href="#kapcho-eat">Меню</a></li>
-                    <li><a href="#kapcho-contacts">Контакты</a></li>
-                    <li><a href="#">Заказать стол</a></li>
+                    <li><a class="page-scroll" href="#cho-v-kapcho">Чо в Капчо?</a></li>
+                    <li><a class="page-scroll" href="#kapcho-gallery">Галерея</a></li>
+                    <li><a class="page-scroll" href="#kapcho-eat">Меню</a></li>
+                    <li><a class="page-scroll" href="#kapcho-contacts">Контакты</a></li>
+                    <li><a>Заказать стол</a></li>
                 </ul>
             </div>
 
@@ -209,8 +209,47 @@
 
 </section>
 
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 copyright">© Все права защищены</div>
+            <div class="footer-icons col-sm-4">
+                <a href="https://www.facebook.com/kapchobar" title="Facebook" target="_blank" class="kap-block-footer-ico">
+                    <svg id="Capa_1" style="enable-background:new 0 0 112.196 112.196;" version="1.1" viewBox="0 0 112.196 112.196" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+                        <g>
+                            <circle cx="56.098" cy="56.098" r="56.098" class="radius" />
+                            <path class="logo" d="M70.201,58.294h-10.01v36.672H45.025V58.294h-7.213V45.406h7.213v-8.34   c0-5.964,2.833-15.303,15.301-15.303L71.56,21.81v12.51h-8.151c-1.337,0-3.217,0.668-3.217,3.513v7.585h11.334L70.201,58.294z" />
+                        </g>
+                    </svg>
+                </a>
+                <a href="https://www.instagram.com/kapchobar" title="Instagram" target="_blank" class="kap-block-footer-ico">
+                    <svg enable-background="new 0 0 128 128" height="128px" id="Layer_1" version="1.1"
+                            viewBox="0 0 128 128" width="128px" xml:space="preserve"
+                            xmlns="http://www.w3.org/2000/svg">
+                        <g>
+                            <circle cx="64" cy="64" class="radius" r="64"/>
+                        </g>
+                        <g>
+                            <path class="logo" d="M91.77,28H36.229C31.702,28,28,31.703,28,36.229v55.542c0,4.525,3.702,8.229,8.229,8.229h55.543   c4.525,0,8.229-3.703,8.229-8.229V36.229C100,31.703,96.297,28,91.77,28z M51.26,58.171c2.239-4.792,7.101-8.114,12.74-8.114   c5.64,0,10.5,3.322,12.74,8.114c0.844,1.806,1.318,3.818,1.318,5.943c0,7.763-6.295,14.057-14.058,14.057   c-7.764,0-14.058-6.294-14.058-14.057C49.941,61.99,50.416,59.977,51.26,58.171z M91.599,89.371c0,1.132-0.925,2.057-2.058,2.057   H38.457c-1.132,0-2.057-0.925-2.057-2.057v-31.2h6.111c-0.526,1.912-0.797,3.906-0.797,5.943c0,5.953,2.318,11.549,6.528,15.758   c4.209,4.21,9.806,6.526,15.758,6.526s11.549-2.317,15.758-6.526c4.209-4.21,6.527-9.806,6.527-15.759   c0-2.037-0.271-4.031-0.797-5.943h6.11V89.371L91.599,89.371z M91.599,47.543c0,1.509-1.233,2.744-2.743,2.744h-8.229   c-1.509,0-2.743-1.235-2.743-2.744v-8.228c0-1.509,1.234-2.743,2.743-2.743h8.229c1.509,0,2.742,1.234,2.742,2.743V47.543   L91.599,47.543z"/>
+                        </g>
+                    </svg>
+                </a>
+                <a href="https://vk.com/kapchobar" title="VK" target="_blank" class="kap-block-footer-ico">
+                    <svg id="Capa_1" style="enable-background:new 0 0 112.196 112.196;" version="1.1" viewBox="0 0 112.196 112.196" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+                        <g>
+                            <g>
+                                <circle cx="56.098" cy="56.098" r="56.098" class="radius"/>
+                            </g>
+                            <path class="logo" d="M53.979,80.702h4.403c0,0,1.33-0.146,2.009-0.878   c0.625-0.672,0.605-1.934,0.605-1.934s-0.086-5.908,2.656-6.778c2.703-0.857,6.174,5.71,9.853,8.235   c2.782,1.911,4.896,1.492,4.896,1.492l9.837-0.137c0,0,5.146-0.317,2.706-4.363c-0.2-0.331-1.421-2.993-7.314-8.463   c-6.168-5.725-5.342-4.799,2.088-14.702c4.525-6.031,6.334-9.713,5.769-11.29c-0.539-1.502-3.867-1.105-3.867-1.105l-11.076,0.069   c0,0-0.821-0.112-1.43,0.252c-0.595,0.357-0.978,1.189-0.978,1.189s-1.753,4.667-4.091,8.636c-4.932,8.375-6.904,8.817-7.71,8.297   c-1.875-1.212-1.407-4.869-1.407-7.467c0-8.116,1.231-11.5-2.397-12.376c-1.204-0.291-2.09-0.483-5.169-0.514   c-3.952-0.041-7.297,0.012-9.191,0.94c-1.26,0.617-2.232,1.992-1.64,2.071c0.732,0.098,2.39,0.447,3.269,1.644   c1.135,1.544,1.095,5.012,1.095,5.012s0.652,9.554-1.523,10.741c-1.493,0.814-3.541-0.848-7.938-8.446   c-2.253-3.892-3.954-8.194-3.954-8.194s-0.328-0.804-0.913-1.234c-0.71-0.521-1.702-0.687-1.702-0.687l-10.525,0.069   c0,0-1.58,0.044-2.16,0.731c-0.516,0.611-0.041,1.875-0.041,1.875s8.24,19.278,17.57,28.993   C44.264,81.287,53.979,80.702,53.979,80.702L53.979,80.702z"/>
+                        </g>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <script src="/js/app.js"></script>
 <!--<script src="https://www.jscache.com/wejs?wtype=cdsscrollingravewide&amp;uniq=128&amp;locationId=12104938&amp;lang=ru&amp;border=false&amp;shadow=true&amp;display_version=2"></script>-->
-<!--<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;key=AIzaSyBQczVEHYVtk0-Y4XeWgvf52hEvM_xLU9o&amp;language=ru"></script>-->
 </body>
 </html>
